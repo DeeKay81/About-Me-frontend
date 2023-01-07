87dc626934d98dffa83cea75b94e4fee2478918e
DeeKay81 <dany.kratochwil@gmail.com>
2023-01-07 13:42:58 +0100

    Updates

---
+++ precache-manifest.cc49fdb2411f86db06bc4c8e5847118f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d23829c84695210d698",
+    "url": "/DeeKay81/About-Me/static/css/main.3bbf1b08.chunk.css"
+  },
+  {
+    "revision": "5d23829c84695210d698",
+    "url": "/DeeKay81/About-Me/static/js/main.5d23829c.chunk.js"
+  },
+  {
+    "revision": "4a4c21dda33e7451eb2b",
+    "url": "/DeeKay81/About-Me/static/css/1.053ce20b.chunk.css"
+  },
+  {
+    "revision": "4a4c21dda33e7451eb2b",
+    "url": "/DeeKay81/About-Me/static/js/1.4a4c21dd.chunk.js"
+  },
+  {
+    "revision": "7e12c081a0e236d8db0b",
+    "url": "/DeeKay81/About-Me/static/js/runtime~main.7e12c081.js"
+  },
+  {
+    "revision": "e6a0ec3272827e77d0d76e22691d5d77",
+    "url": "/DeeKay81/About-Me/static/media/project1-img.e6a0ec32.jpg"
+  },
+  {
+    "revision": "8be692f369d6017b5d1c6fadd680af9a",
+    "url": "/DeeKay81/About-Me/static/media/devRock.8be692f3.jpg"
+  },
+  {
+    "revision": "f132fb2fa2a328563e4937e2889ac211",
+    "url": "/DeeKay81/About-Me/static/media/CV.f132fb2f.pdf"
+  },
+  {
+    "revision": "f35599834317c87dbf070f12bce0081e",
+    "url": "/DeeKay81/About-Me/static/media/profilePic.f3559983.jpg"
+  },
+  {
+    "revision": "726123682e7738029a81c47700502c48",
+    "url": "/DeeKay81/About-Me/static/media/bee.72612368.png"
+  },
+  {
+    "revision": "9bbed92c4d957ade79df34d7242c6a0f",
+    "url": "/DeeKay81/About-Me/static/media/cat.9bbed92c.jpg"
+  },
+  {
+    "revision": "aec87a26601f940d4c0f3cd0907dcb66",
+    "url": "/DeeKay81/About-Me/static/media/dog.aec87a26.png"
+  },
+  {
+    "revision": "3071e4092fe28566fa51986a0780bcd1",
+    "url": "/DeeKay81/About-Me/static/media/pig.3071e409.jpeg"
+  },
+  {
+    "revision": "a4a40b487fc2d8ecb512391cc742cd76",
+    "url": "/DeeKay81/About-Me/static/media/violett.a4a40b48.jpg"
+  },
+  {
+    "revision": "6c6bc8decfcbf5cd39ef9f022f7d2d61",
+    "url": "/DeeKay81/About-Me/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/DeeKay81/About-Me/precache-manifest.cc49fdb2411f86db06bc4c8e5847118f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/DeeKay81/About-Me/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
