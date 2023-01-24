79a58bec66a993c473ab722d4f303d3a697c5c5a
DeeKay81 <dany.kratochwil@gmail.com>
2023-01-24 11:03:29 +0100

    python project works, js projects almost done

--- src/components/projects/CentipedeIndex.jsx
+++ src/components/projects/CentipedeIndex.jsx
@@ -1,21 +1,9 @@
-// let centipedeIndexFile = require('../../../src/cindex.html');
-
-// export default function CentipedeIndex() {
-
-//     return (
-//         <div dangerouslySetInnerHTML={ {__html: centipedeIndexFile} } />
-//     );
-// }
-
 import React, { Component } from 'react';
 import Page from '../../projects/Centepede/index.html';
-var htmlDoc = {__html: Page};
+
+let htmlDoc = {__html: Page};
 
 export default class CentipedeIndex extends Component {
-  constructor(props){
-    super(props);
-  }
-
   render(){
      return (<div dangerouslySetInnerHTML={htmlDoc} />)
 }}
